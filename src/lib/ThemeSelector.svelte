1da64a6ac6a3d3a91b985190b9f5ea826a610088
Samuel Middendorp <samiezaurus@gmail.com>
2023-05-01 22:30:06 +0200

    Begin work on theme selecting

--- src/lib/ThemeSelector.svelte
+++ src/lib/ThemeSelector.svelte
@@ -9,6 +9,7 @@
             { property: "text-200", value: "#efefef" },
             { property: "accent", value: "#be3455" },
             { property: "color-100", value: "#120407" },
+            { property: "color-500", value: "#210a0f" },
         ],
     };
 
@@ -24,25 +25,82 @@
     onMount(() => {
         applyTheme(darkTheme);
     });
-    </script>
+</script>
+
 <body>
-<svg class="theme-selector-icon" width="100" height="100" version="1.1" viewBox="0 0 8.8088694 9.7604094" xmlns="http://www.w3.org/2000/svg">
- <g transform="translate(-101.61459,-133.36458)">
-  <path class="theme-selector-icon-fill" d="m101.86458 138.64167h7.9375l-3.96875 3.96875z"/>
-  <path class="theme-selector-icon-line" d="m105.83333 134.9375-1.32291-1.32292m1.32291 1.32292 3.96875 3.96875-3.96875 3.96875-3.96875-3.96875z" stroke-linecap="round" stroke-linejoin="round" stroke-width=".5"/>
-  <path class="theme-selector-icon-droplet" d="m109.80208 140.22917s0.62138 0.75356 0.62138 1.15915c1e-5 0.4056-0.21579 0.64312-0.62138 0.64311-0.4056 0-0.59966-0.23751-0.59966-0.64311 0-0.40559 0.59966-1.15915 0.59966-1.15915z"/>
- </g>
-</svg>
+    <div class="theme-selector">
+        <svg
+            class="theme-selector-icon"
+            width="100%"
+            height="100%"
+            version="1.1"
+            viewBox="0 0 8.8088694 9.7604094"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g transform="translate(-101.61459,-133.36458)">
+                <path
+                    class="theme-selector-icon-fill"
+                    d="m101.86458 138.64167h7.9375l-3.96875 3.96875z"
+                />
+                <path
+                    class="theme-selector-icon-line"
+                    d="m105.83333 134.9375-1.32291-1.32292m1.32291 1.32292 3.96875 3.96875-3.96875 3.96875-3.96875-3.96875z"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width=".5"
+                />
+                <path
+                    class="theme-selector-icon-droplet"
+                    d="m109.80208 140.22917s0.62138 0.75356 0.62138 1.15915c1e-5 0.4056-0.21579 0.64312-0.62138 0.64311-0.4056 0-0.59966-0.23751-0.59966-0.64311 0-0.40559 0.59966-1.15915 0.59966-1.15915z"
+                />
+            </g>
+        </svg>
+        <div class="theme-selector-selectors">
+            <div class="theme-selector-selector">
+            </div>
+        </div>
+    </div>
 </body>
+
 <style>
-    .theme-selector-icon-line{
-        stroke: #efefef;
+    .theme-selector{
+        background-color: var(--text-200);
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        overflow: hidden;
+        height: 4vw;
+        width: auto;
+        place-content: center;
+        display: grid;
+        grid-template-columns: 4vw min-content;
+        padding: 0.4rem;
+        border-top-right-radius: 0.5rem;
+
+    }
+    .theme-selector-selectors{
+        display: grid;
+    }
+    .theme-selector:hover .theme-selector-selectors{
+    }
+    .theme-selector-selector{
+        background-color: #ff0000;
+        margin-left: 1rem;
+        width: 2vw;
+        height: 2vw;
+    }
+    .theme-selector-icon{
+        max-width: 50px;
+        max-height: 50px;
+    }
+    .theme-selector-icon-line {
+        stroke: var(--color-500);
         fill: none;
     }
-    .theme-selector-icon-fill{
-        fill: #ff0000;
+    .theme-selector-icon-fill {
+        fill: var(--color-500);
     }
-    .theme-selector-icon-droplet{
-        fill: #ff0000;
+    .theme-selector-icon-droplet {
+        fill: var(--color-500);
     }
 </style>
